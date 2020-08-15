500fa22d3c7fdf89dcead855bef44a9f5075ff4a
Damian Pieszczyński <razzededge@gmail.com>
2020-08-16 01:10:45 +0200

    html

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,21 @@
-<h1>Test</h1>
+<script>
+    import Timer from './Timer.svelte';
+    import HowTo from './HowTo.svelte';
+</script>
+
+<style>
+ h1, h3 {
+     text-align: center;
+
+ }
+</style>
+
+<h1>Handwashing app</h1>
+
+<Timer />
+
+<HowTo />
+
+<h3>
+<a href="http://who.int">Picture source WHO</a>
+</h3>
---
+++ src/HowTo.svelte
@@ -0,0 +1,9 @@
+<style>
+img {
+    max-width: 100%;
+}
+</style>
+
+<div bp="grid">
+    <img bp="offset-5@md 4@md 12@sm" src="assets/how_to_handwash_lge.gif" alt="Wash hands">
+</div>
